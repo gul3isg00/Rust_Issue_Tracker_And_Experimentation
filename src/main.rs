4af0a386e5ed7fcd2854a6ba44cef3905df824be
aiden <113120503+gul3isg00@users.noreply.github.com>
2024-10-04 13:39:05 +0100

    Setup basic gui application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use eframe::egui;
+
 enum ItemPriority {
     Low,
     Medium,
@@ -22,14 +25,13 @@ type DateTime = String;
 struct ItemComment {
     commenter: String,
     message: String,
-    time_created: DateTime
+    time_created: DateTime,
 }
 
-struct TodoItem {
+struct Issue {
     name: String,
     description: String,
     reporter: String,
-    reviewer: String,
     comment_thread: Vec<ItemComment>,
     priority: ItemPriority,
     status: ItemStatus,
@@ -43,24 +45,63 @@ struct User {
     user_type: UserType,
 }
 
-fn main() {
-    let default_admin = User {
-        email: String::from("admin@test.com"),
-        password: String::from("encrypted-password"),
-        username: String::from("TheBeesKnees"),
-        user_type: UserType::Admin,
+fn main() -> eframe::Result {
+    env_logger::init();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
     };
-
-    let default_client = User {
-        email: String::from("client@test.com"),
-        password: String::from("encrypted-password"),
-        username: String::from("BiteAtTheHand"),
-        user_type: UserType::Client,
-    };
-
-    println!("{}", default_admin.email);
+    eframe::run_native(
+        "Rust Issue Tracker",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Ok(Box::<ClientApp>::default())
+        }),
+    )
 }
 
-// USE A STRUCT
+// Defines the variables stored within the app
+struct ClientApp {
+    users: Vec<User>,
+    issues: Vec<Issue>,
+    name: String,
+    age: u32,
+}
+
+// Instantiates the default variables.
+impl Default for ClientApp {
+    fn default() -> Self {
+        Self {
+            users: Vec::new(),
+            issues: Vec::new(),
+            name: "Aiden".to_owned(),
+            age: 21,
+        }
+    }
+}
+
+// Defines the interface and interactions
+impl eframe::App for ClientApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Rust Issue Tracker");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+            // ui.image(egui::include_image!(
+            // ));
+        });
+    }
+}
+
 // USE IMPL
 // USE A COMPOSITE
