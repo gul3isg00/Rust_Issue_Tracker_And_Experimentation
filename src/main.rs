ef2f0c90baef5f1398fcc6b2ab4794c96a17d17c
aiden <113120503+gul3isg00@users.noreply.github.com>
2024-10-04 16:44:11 +0100

    Finish form and allow user to show and hide form

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use eframe::egui;
-use egui::Response;
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use chrono::Local;
+use eframe::egui;
+use std::fmt;
 
+#[derive(PartialEq, Eq, Clone)]
 enum ItemPriority {
+    NA,
     Low,
     Medium,
     High,
     Critical,
+}
+
+impl fmt::Display for ItemPriority {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ItemPriority::NA => write!(f, "NA"),
+            ItemPriority::Low => write!(f, "Low"),
+            ItemPriority::Medium => write!(f, "Medium"),
+            ItemPriority::High => write!(f, "High"),
+            ItemPriority::Critical => write!(f, "Critical"),
+        }
+    }
 }
 
 enum ItemStatus {
@@ -50,7 +64,7 @@ struct User {
 fn main() -> eframe::Result {
     env_logger::init();
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([500.0, 500.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -67,9 +81,11 @@ fn main() -> eframe::Result {
 struct ClientApp {
     users: Vec<User>,
     issues: Vec<Issue>,
-    loggedInAs: usize,
+    logged_in_as: usize,
     cur_ticket_message: String,
-    cur_ticket_name: String
+    cur_ticket_priority: ItemPriority,
+    cur_ticket_name: String,
+    show_ticket_form: bool,
 }
 
 // Instantiates the default variables.
@@ -90,10 +106,12 @@ impl Default for ClientApp {
                     user_type: UserType::Admin,
                 },
             ]),
-            loggedInAs: 0,
+            logged_in_as: 0,
             cur_ticket_message: String::from(""),
             cur_ticket_name: String::from(""),
+            cur_ticket_priority: ItemPriority::NA,
             issues: Vec::new(),
+            show_ticket_form: false,
         }
     }
 }
@@ -101,31 +119,72 @@ impl Default for ClientApp {
 // Defines the interface and interactions
 impl eframe::App for ClientApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Rust Issue Tracker");
             ui.add(egui::Label::new(format!(
                 "Currently logged in as: {}",
-                self.users[self.loggedInAs].username
+                self.users[self.logged_in_as].username
             )));
-            let ticket_name_label = ui.label("Ticket title: ");
-            let ticket_name_input: egui::Response = ui.add(egui::TextEdit::singleline(&mut self.cur_ticket_name)).labelled_by(ticket_name_label.id);
-            let ticket_input: egui::Response = ui.add(egui::TextEdit::multiline(&mut self.cur_ticket_message));
-            let ticket_button: egui::Response = ui.button("Raise new ticket");
-            if ticket_button.clicked() {
-                self.issues.push(Issue{
-                    name: String::from(self.cur_ticket_name.clone()),
-                    description: String::from(self.cur_ticket_message.clone()),
-                    reporter: String::from(self.users[self.loggedInAs].email.clone()),
-                    comment_thread: Vec::new(),
-                    priority: ItemPriority::Critical,
-                    status: ItemStatus::New,
-                    time_created: Local::now().to_string(),
-                });
+
+            let mut show_text = String::from("Raise new");
+
+            if(self.show_ticket_form){show_text = String::from("Discard")}
+
+            let create_ticket_button: egui::Response = ui.button(format!("{} ticket", show_text));
+            if create_ticket_button.clicked() {
+                self.show_ticket_form = !self.show_ticket_form;           
             }
 
-            // ui.image(egui::include_image!(
-            // ));
+            if (self.show_ticket_form) {
+                ui.horizontal(|ui| {
+                    let ticket_name_label = ui.label("Ticket title: ");
+                    ui.text_edit_singleline(&mut self.cur_ticket_name)
+                        .labelled_by(ticket_name_label.id);
+                });
+
+                ui.add(egui::TextEdit::multiline(&mut self.cur_ticket_message));
+
+                egui::ComboBox::from_label("Priority:")
+                    .selected_text(self.cur_ticket_priority.to_string())
+                    .show_ui(ui, |ui| {
+                        ui.selectable_value(
+                            &mut self.cur_ticket_priority,
+                            ItemPriority::Low,
+                            "Low",
+                        );
+                        ui.selectable_value(
+                            &mut self.cur_ticket_priority,
+                            ItemPriority::Medium,
+                            "Medium",
+                        );
+                        ui.selectable_value(
+                            &mut self.cur_ticket_priority,
+                            ItemPriority::High,
+                            "High",
+                        );
+                        ui.selectable_value(
+                            &mut self.cur_ticket_priority,
+                            ItemPriority::Critical,
+                            "Critical",
+                        );
+                    });
+
+                let ticket_button: egui::Response = ui.button("Submit ticket");
+
+                if ticket_button.clicked() {
+                    if self.cur_ticket_priority != ItemPriority::NA {
+                        self.issues.push(Issue {
+                            name: String::from(self.cur_ticket_name.clone()),
+                            description: String::from(self.cur_ticket_message.clone()),
+                            reporter: String::from(self.users[self.logged_in_as].email.clone()),
+                            comment_thread: Vec::new(),
+                            priority: self.cur_ticket_priority.clone(),
+                            status: ItemStatus::New,
+                            time_created: Local::now().to_string(),
+                        });
+                    }
+                }
+            }
         });
     }
 }
