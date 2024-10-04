dfd8a9a97ce549c19d7a9d93fe6cdc47871b1204
aiden <113120503+gul3isg00@users.noreply.github.com>
2024-10-04 14:20:41 +0100

    setup basic client view

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ fn main() -> eframe::Result {
 struct ClientApp {
     users: Vec<User>,
     issues: Vec<Issue>,
-    name: String,
+    loggedInAs: usize,
     age: u32,
 }
 
@@ -73,9 +73,22 @@ struct ClientApp {
 impl Default for ClientApp {
     fn default() -> Self {
         Self {
-            users: Vec::new(),
+            users: Vec::from([
+                User {
+                    email: String::from("admin@test.com"),
+                    password: String::from("encrypted-password"),
+                    username: String::from("TheBeesKnees"),
+                    user_type: UserType::Admin,
+                },
+                User {
+                    email: String::from("client@test.com"),
+                    password: String::from("encrypted-password"),
+                    username: String::from("BiteAtTheHand"),
+                    user_type: UserType::Client,
+                },
+            ]),
+            loggedInAs: 0,
             issues: Vec::new(),
-            name: "Aiden".to_owned(),
             age: 21,
         }
     }
@@ -87,15 +100,15 @@ impl eframe::App for ClientApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Rust Issue Tracker");
             ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
+                let name_label = ui.label("Currently logged in as: ");
+                ui.text_edit_singleline(&mut self.users[self.loggedInAs].username)
                     .labelled_by(name_label.id);
             });
             ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
             if ui.button("Increment").clicked() {
                 self.age += 1;
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            ui.label(format!("Age {}",self.age));
 
             // ui.image(egui::include_image!(
             // ));
