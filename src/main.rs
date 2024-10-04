7f9b31d49cdd3b06f750567d01ac6bf0d518f949
aiden <113120503+gul3isg00@users.noreply.github.com>
2024-10-04 12:26:26 +0100

    Setup type and structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+enum ItemPriority {
+    Low,
+    Medium,
+    High,
+    Critical,
+}
+
+enum ItemStatus {
+    New,
+    Open,
+    Closed,
+}
+
+#[allow(clippy::upper_case_acronyms)]
+type DateTime = String;
+
+struct TodoItem {
+    name: String,
+    description: String,
+    reporter: String,
+    reviewer: String,
+    priority: ItemPriority,
+    status: ItemStatus,
+    time_created: DateTime,
+}
+
 fn main() {
     println!("Hello, world!");
 }
